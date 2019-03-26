445ae78bcddd7e974ae626f64f8118352a40cb5a
Shandra <shandradev85@gmail.com>
2019-03-26 16:36:13 -0600

    added client side

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,12 +1,12 @@
 class Api::ItemsController < ApplicationController
-  # before_action :set_item, only: [ :show, :destroy, :update, :edit]
+  # before_action :set_item, only: [:show, :destroy, :update, :edit]
 
-  #read
+  # Read
   def index
     render json: Item.all
   end
 
-  #create
+  # create
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -16,32 +16,30 @@ class Api::ItemsController < ApplicationController
     end
   end
 
-  #update
+  # update
   def update
-  #  if @item.update(item_params)
-  #   render json: @item
-  #  else
-  #   render json: { errors: @item.errors }, status: :unprocessable_entity
-  #  end
-    item = Item.find(params_id)
+    # if @item.update(item_params)
+    #   render json: @item
+    # else
+    #   render json: { errors: @item.errors }, status: :unprocessable_entity
+    # end
+    item = Item.find(params[:id])
     item.update(complete: !item.complete)
     render json: item
   end
 
   #delete
-  def destroy 
+  def destroy
     Item.find(params[:id]).destroy
-    render json: { message: 'Item is deleted'}
-
+    render json:  { message: 'item is deleted'}
   end
 
   private
+    def item_params
+      params.require(:item).permit(:name, :complete)
+    end
 
     # def set_item
     #   @item = Item.find(params[:id])
     # end
-
-    def item_params
-      params.require(:item).permit(:name, :complete)
-    end
 end
